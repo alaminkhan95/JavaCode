4bc93ef73ffaff63717c3f48b8b8a54dc4f02815
alamin khan <alaminkhanbd71@gmail.com>
2024-12-06 20:39:18 -0600

    added new code

---
+++ src/test/java/array/SumOfTwo.java
@@ -0,0 +1,33 @@
+package array;
+
+public class SumOfTwo {
+
+    public static void main(String[] args) {
+
+        int[] numArr = {13, 43, 2, 71};
+
+        int num = 84;
+
+        int[] numbers = new int[2];
+
+        for (int i =0;i<numArr.length;i++){
+
+            for (int j =i+1;j<numArr.length;j++){
+
+                if (numArr[i]+numArr[j]==num){
+
+                    numbers[0]=numArr[i];
+                    numbers[1]= numArr[j];
+
+                }
+            }
+        }
+
+        for (int number : numbers){
+
+            System.out.println(number);
+        }
+
+
+    }
+}
